04a4910d8a30ab029ff938a467be18b131981764
wachirwait <wachirawit.cho@rmutto.ac.th>
2024-10-14 18:17:56 +0700

    1

---
+++ app/routes/lab02.petDetail.$petId.tsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "@remix-run/react";
+
+
+export default function HerbDetail() {
+    const myParams = useParams();
+    const petId = myParams.petId;
+    const [petData, setPetData] = useState({
+        petId: "",
+        petName: "",
+        petBD: "",
+        petType: "",
+        petNote: "",
+        petOwner: ""
+    });
+
+    useEffect(() => {
+        try {
+            const fetchPetData = async () => {
+                const petData = await fetch(`http://localhost:3004/api/getPetById/${petId}`);
+                if (petData.ok) {
+                    const petJson = await petData.json();
+                    setPetData(petJson);
+                    console.log(petJson);
+                } else {
+                    alert('[ERR] Failed to loaded data.');
+                }
+            }
+
+            fetchPetData().catch(console.error);
+        } catch (error) {
+            alert('[ERR] An error occurred while loading the data.');
+        }
+    }, []);
+
+
+
+    return (
+        <div className="m-3">
+            <a href='#'>[ ข้อมูลสัตว์เลี้ยง ]</a>
+            <h1 className="font-bold">รายละเอียดสัตว์เลี้ยง</h1>
+            {
+                <div key={petData.petId}>
+                    <div className="font-bold p-2 m-2 border-2 rounded-lg">
+                        ชื่อสัตว์เลี้ยง: {petData.petName}<br />
+                        รายละเอียด: {petData.petNote}<br />
+                        ประเภท: {petData.petType}<br />
+                        วันเกิด: {petData.petBD}<br />
+                        เจ้าของ: {petData.petOwner}<br />
+                    </div>
+                </div>
+            }
+            <a href='/lab02/petLists'>[ ย้อนกลับ ]</a>
+        </div>
+    );
+}
